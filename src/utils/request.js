b1a07656be8f5410055c89392a03d7fab048ca5e
杨可可 <yangkeke@mmuu.com>
2017-12-03 15:24:43 +0800

    1.0.2

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,60 +2,69 @@
 /**
  * network request
  */
-const request = {
+class Request {
 
-	get(url, params) {
-		params = params || {};
+    _params = {};
 
-		console.log('==============================================');
-		console.log(`Get:${url}`);
-		console.log('Params:', params);
+    addParam(name, value) {
+        this._params[name] = value;
+    }
 
-		return new Promise(((resolve, reject) => {
-			$.ajax({
-				type: 'GET',
-				url,
-				data: params,
-				cache: false,
-				dataType: 'json',
-				success: (result) => {
-					console.log('Result:', result);
-					resolve(result);
-				},
-				error: (XMLHttpRequest, textStatus, errorThrown) => {
-					console.log('Error:', XMLHttpRequest, textStatus, errorThrown);
-					reject(XMLHttpRequest);
-				}
-			});
-		}));
-	},
+    get(url, params) {
 
-	post(url, params) {
-		params = params || {};
+        params = Object.assign(params || {}, this._params);
 
-		console.log('==============================================');
-		console.log('Post:', url);
-		console.log('Data:', params);
+        console.log('==============================================');
+        console.log(`Get:${url}`);
+        console.log('Params:', params);
 
-		return new Promise((resolve, reject) => {
-			$.ajax({
-				type: 'POST',
-				url,
-				data: params,
-				cache: false,
-				dataType: 'json',
-				success: (result) => {
-					console.log('Result:', result);
-					resolve(result);
-				},
-				error: (XMLHttpRequest, textStatus, errorThrown) => {
-					console.log('Error:', XMLHttpRequest, textStatus, errorThrown);
-					reject(XMLHttpRequest);
-				}
-			});
-		});
-	}
+        return new Promise(((resolve, reject) => {
+            $.ajax({
+                type: 'GET',
+                url,
+                data: params,
+                cache: false,
+                dataType: 'json',
+                success: (result) => {
+                    console.log('Result:', result);
+                    resolve(result);
+                },
+                error: (XMLHttpRequest, textStatus, errorThrown) => {
+                    console.log('Error:', XMLHttpRequest, textStatus, errorThrown);
+                    reject(XMLHttpRequest);
+                }
+            });
+        }));
+    }
 
-};
+    post(url, params) {
 
+        params = Object.assign(params || {}, this._params);
+
+        console.log('==============================================');
+        console.log('Post:', url);
+        console.log('Data:', params);
+
+        return new Promise((resolve, reject) => {
+            $.ajax({
+                type: 'POST',
+                url,
+                data: params,
+                cache: false,
+                dataType: 'json',
+                success: (result) => {
+                    console.log('Result:', result);
+                    resolve(result);
+                },
+                error: (XMLHttpRequest, textStatus, errorThrown) => {
+                    console.log('Error:', XMLHttpRequest, textStatus, errorThrown);
+                    reject(XMLHttpRequest);
+                }
+            });
+        });
+    }
+
+}
+
+const request = new Request();
 export default request;
