75a914d5a5aa733f074a3d0c22fca1624d744656
杨可可 <yangkeke@mmuu.com>
2017-12-08 13:33:12 +0800

    1.0.5

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import utils from './utils/utils';
 import dateUtils from "./utils/dateUtils";
 import urlUtils from "./utils/urlUtils";
 import pinyin from "./utils/pinyin";
+import regexp from './utils/regexp';
 
 import sessionStore from "./store/sessionStore";
 import localStore from "./store/localStore";
 
 export {
-    validator, platform, request, utils, dateUtils, urlUtils, pinyin,
+    validator, platform, request, utils, dateUtils, urlUtils, pinyin, regexp,
     localStore, sessionStore
 }
