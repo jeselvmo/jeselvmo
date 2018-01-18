4c99d54e4aec5bda9ca2335a01ebc78f69f2e793
杨可可 <yangkeke@mmuu.com>
2018-01-18 17:38:30 +0800

    1.0.7

--- src/mobile/eruda.js
+++ src/mobile/eruda.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 export default function () {
 	let script = document.createElement('script');
-	script.src = "http://cdn.bootcss.com/eruda/1.2.4/eruda.min.js";
+	script.src = "https://cdn.bootcss.com/eruda/1.2.4/eruda.min.js";
 	document.body.appendChild(script);
 	script.onload = function () {
 		eruda.init();
